7a5520d2994114a2f708e9aaf6934f00afd4c787
“SashaYuu” <krarmohamed65@gmail.com>
2023-11-07 17:35:57 +0100

    finally done

---
+++ arrays/src/main.js
@@ -0,0 +1,43 @@
+
+/*
+let students = ["sami", "alex", "teo", "jack", "fin", "jojo"]
+
+stundets.push("emma") //adds a name to the array
+students.pop()//delets the last string of the array 
+students[1]= "moana" //changes alex name with moana
+students.splice(3, 1, "layla") // deletes the name on the postion 3 and the 1 there is saying only it delete it nothing after it if we weouldve wrote 2 it will delete jack and fin cuz we said 2, layla will be changed instead of jack NOTE WE CAN WRITE JUST THE 3 AND THAT WILL BE ENOUGH FOR THE PARAMETER 
+students.splice(3, 0, "layla")// add layla so it become second postion string
+
+
+let num = [10,20,30,40,50,60,70,80,90]
+console.log(num.includes(3)) //asking wether 3 is in the array or not
+console.log(num.indexOf(20));//check the index postion of 20
+console.log(num.reverse());//revers the strings
+
+*/
+window.onload = function() {
+    let start = "karrar"
+    console.log(start) 
+}
+
+let names =[];
+
+document.getElementById("sub").addEventListener("click",submit);
+
+function submit(){
+    let studentName = document.getElementById("name").value
+    names.push(studentName);
+    document.getElementById("h").innerHTML = names;
+    console.log(names);
+    
+
+}
+
+document.getElementById("del").addEventListener("click",del);
+function del(){
+    names.pop()
+    document.getElementById("h").innerHTML = names;
+
+}
+
+
---
+++ javaa/java/script/main.js
@@ -0,0 +1,24 @@
+document.getElementById("res").innerHTML = "hello papa"
+document.getElementById("js").onclick = function (){ document.getElementById("sami").innerHTML = "goodmornininngingn"
+
+}
+
+
+
+let isWarm = "hot"
+let isCold = "cold"
+
+let weather = ("the weather today is" + isWarm && isCold)
+console.log(weather)
+
+
+let temp = 30
+
+if (temp >= 30){
+    console.log("its a hot day today")
+}
+
+
+
+let socer = 13;
+if (socer )
