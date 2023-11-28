f3ecf4224b84d78d61f40be1e52640e5cf76bfe1
“SashaYuu” <krarmohamed65@gmail.com>
2023-11-28 23:37:33 +0100

    Add OMDBAPI movie search and display functionality

--- fetch/src/js/main.js
+++ fetch/src/js/main.js
@@ -16,8 +16,8 @@ menu.then((data)=>{
     console.log(data)
 }) */
 
-
-fetch("http://www.omdbapi.com/?apikey=416ed51a&s=star")
+/* 
+fetch("http://www.omdbapi.com/?apikey=7d88fbef&s=star")
 
     .then((response) => response.json())
     .then((data) => {
@@ -35,14 +35,44 @@ fetch("http://www.omdbapi.com/?apikey=416ed51a&s=star")
 fetch("https://fakestoreapi.com/products")
   .then((response) => response.json())
   .then((data) => {
-    console.log(data);
 
     for (let i = 0; i < data.length; i++) {
       if (data[i].price < 50) {
         console.log(data[i].title, data[i].price);
       }
     }
-  });
+  }); */
+
+
+fetch("http://www.omdbapi.com/?apikey=7d88fbef&s=death&y=2000")
+
+    .then((response) => response.json())
+    .then((data) => {
+
+        for (let i = 0; i < data.Search.length; i++) {
+            console.log(data.Search[i].Title);
+        }
+
+        const datasContainer = document.getElementById("movies");
+        
+        for (let i = 0; i < data.Search.length; i++) {
+          const dataContainer = document.createElement("div");
+          const title = document.createElement("h3");
+        
+          dataContainer.className = "data";
+          dataContainer.addEventListener("click", () => {
+            dataClicker(data.Search[i].Title);
+          });
+          title.innerHTML = data.Search[i].Title;
+        
+          dataContainer.appendChild(title);
+          datasContainer.appendChild(dataContainer);
+        }
+    });
+
+const dataClicker = (dataClicked) => {
+  console.log("you chose the movie:", dataClicked);
+};
 
 
 
